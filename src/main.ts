9601dc9c8a7b3b99149ce85ecae9add3a52c02e7
Sina Sedaghat <sina@Sinas-MacBook-Pro.local>
2024-01-09 14:54:42 +0330

    add i18n to main.ts for global use

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,13 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
+import i18n from './plugins/i18n'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(vuetify)
+app.use(i18n)
 
 app.mount('#app')
