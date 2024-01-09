89ea2bb08f369f45096fab7f04d464b86206d214
Sina Sedaghat <sina@Sinas-MacBook-Pro.local>
2024-01-09 14:48:27 +0330

    update i18n confige

--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -2,14 +2,20 @@ import { createI18n } from 'vue-i18n'
 import en from '@/locales/en'
 import fa from '@/locales/fa'
 
+const messages = {
+  en: {
+    ...en
+  },
+  fa: {
+    ...fa
+  }
+}
+
 export default createI18n({
   legacy: false,
   locale: 'en',
   fallbackLocale: 'en',
-  messages: {
-    'en': en,
-    'fa': fa
-  },
+  messages: messages,
   rtl: {
     en: false,
     fa: true
