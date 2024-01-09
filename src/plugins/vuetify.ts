c645cf37cb61d75e7066ac4b29232a0a303a2d8b
Sina Sedaghat <sina@Sinas-MacBook-Pro.local>
2024-01-09 14:53:56 +0330

    update vuetify confige and add local adaptor for bind i18n to vuetify

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -6,6 +6,13 @@ import "@mdi/font/css/materialdesignicons.css";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 
+import i18n from './i18n'
+import { useI18n } from 'vue-i18n'
+import { createVueI18nAdapter } from 'vuetify/locale/adapters/vue-i18n'
+
+
+
+
 const lightTheme: ThemeDefinition = {
   dark: false,
   colors: {
@@ -35,6 +42,9 @@ const darkTheme: ThemeDefinition = {
 export default createVuetify({
   components,
   directives,
+  locale: {
+    adapter: createVueI18nAdapter({ i18n, useI18n }),
+  },
   theme: {
     defaultTheme: "light",
     themes: {
