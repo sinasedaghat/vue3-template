1a2e20f67029e315089934c173951cad31fa78f0
Sina Sedaghat <sina@Sinas-MacBook-Pro.local>
2024-01-09 12:36:33 +0330

    add i18n confige and create locale files

---
+++ src/plugins/i18n.ts
@@ -0,0 +1,17 @@
+import { createI18n } from 'vue-i18n'
+import en from '@/locales/en'
+import fa from '@/locales/fa'
+
+export default createI18n({
+  legacy: false,
+  locale: 'en',
+  fallbackLocale: 'en',
+  messages: {
+    'en': en,
+    'fa': fa
+  },
+  rtl: {
+    en: false,
+    fa: true
+  },
+})
