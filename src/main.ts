c83ca3629470e8123de0cb8e85778425936f9835
Sina Sedaghat <sina@Sinas-MacBook-Pro.local>
2024-01-09 12:12:04 +0330

    add i18n in main.ts file for use i18n in project and use it in App.vue

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,14 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import i18n from './plugins/i18n'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(i18n)
 
 app.mount('#app')
+
+
