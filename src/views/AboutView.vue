d28cd553c273280624bc384ed100d212b6f985cd
Sina Sedaghat <sina@Sinas-MacBook-Pro.local>
2024-01-16 11:17:55 +0330

    add scss and preparation of view components

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,6 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
-</template>
+<script setup lang="ts">
+</script>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+<template>
+  <h2 class="title">This is '/about' route bind to AboutView component</h2>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,6 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h2 class="title">This is '/' route bind to HomeView component</h2>
 </template>
