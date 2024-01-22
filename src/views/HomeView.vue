b49c21aa2af60ba409496248cd5cf937a5c69e8b
Sina Sedaghat <sina@Sinas-MacBook-Pro.local>
2024-01-22 17:57:08 +0330

    create basic composable and use it in component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,18 @@
 <script setup lang="ts">
+import { useMouse } from '@/composables/mouse'
+import { computed, ref } from 'vue';
+  const { x, y } = useMouse()
+  const xy = computed(() => x.value + y.value)
 </script>
 
 <template>
   <main>
     <h2 class="title">This is '/' route bind to HomeView component</h2>
+    <p class="subtitle">Mouse position is at: {{ x }}, {{ y }}</p>
+    <div class="chip-green">
+      <div class="bullet">
+        X + Y = {{ xy }}
+      </div>
+    </div>
   </main>
 </template>
