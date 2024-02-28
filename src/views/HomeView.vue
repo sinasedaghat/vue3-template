efc973f93fd8978e45ac00838d78ef0347de4162
Sina Sedaghat <sina@Sinas-MacBook-Pro.local>
2024-02-28 13:43:44 +0330

    start practice for use component and all of features aboute component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,6 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h3 class="title">This is Home Page</h3>
 </template>
